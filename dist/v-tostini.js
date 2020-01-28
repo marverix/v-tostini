a9d6632d179f20e9b6133b90b710647d9693f181
AndrewBogdanovTSS <andrewbogdanov@ukr.net>
2020-01-28 14:48:16 +0300

    Add support for > 2%, ie 11, safari > 9

--- dist/v-tostini.js
+++ dist/v-tostini.js
@@ -1,40 +1,42 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 /* v-tostini v1.1.0 | (c) Marek Sierociński | https://github.com/marverix/v-tostini/blob/master/LICENSE.md */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('vue')) :
-  typeof define === 'function' && define.amd ? define(['vue'], factory) :
-  (global['v-tostini'] = factory(global.vue));
-}(this, (function (Vue) { 'use strict';
+  (typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('vue')) : typeof define === 'function' && define.amd ? define(['vue'], factory) : global['v-tostini'] = factory(global.vue);
+})(void 0, function (Vue) {
+  'use strict';
 
   Vue = Vue && Vue.hasOwnProperty('default') ? Vue['default'] : Vue;
-
   /**
    * Bus
    *
    * Use it to communicate between some Vue instances/components and Tostini Component
    */
-  const bus = new Vue();
 
+  var bus = new Vue();
   /**
    * Component
    *
    * Here will appear all delicious tostinis (or something else :) )
    */
-  const component = {
-    name: 'tostini-plate',
 
-    data() {
+  var component = {
+    name: 'tostini-plate',
+    data: function data() {
       return {
         tostinis: []
       };
     },
-
     methods: {
       /**
        * Add
        * @private
        */
-      _add(config) {
+      _add: function _add(config) {
         this.tostinis.push(config);
+
         this._setTimer(config);
       },
 
@@ -42,8 +44,10 @@
        * Remove
        * @private
        */
-      _remove(id) {
-        const idx = this.tostinis.findIndex(tostini => tostini.id === id);
+      _remove: function _remove(id) {
+        var idx = this.tostinis.findIndex(function (tostini) {
+          return tostini.id === id;
+        });
         this.tostinis.splice(idx, 1);
       },
 
@@ -51,105 +55,85 @@
        * Set timer
        * @private
        */
-      _setTimer(config) {
-        setTimeout(
-          id => {
-            this._remove(id);
-          },
-          config.duration,
-          config.id
-        );
+      _setTimer: function _setTimer(config) {
+        var _this = this;
+
+        setTimeout(function (id) {
+          _this._remove(id);
+        }, config.duration, config.id);
       },
 
       /**
        * Bake
        */
-      bake(config) {
-        const _config = Object.assign(
-          {
-            id: Date.now()
-          },
-          config
-        );
+      bake: function bake(config) {
+        var _config = Object.assign({
+          id: Date.now()
+        }, config);
+
         this._add(_config);
       }
     },
-
-    mounted() {
+    mounted: function mounted() {
       bus.$on('tostini-bake', this.bake);
     },
-
-    render(h) {
-      return h(
-        'div',
-        {
-          class: 'tostini-plate'
-        },
-        this.tostinis.map( (tostini) => {
-          const domProps = {};
-          domProps[tostini.html ? 'innerHTML' : 'innerText'] = tostini.message;
-
-          return h(
-            'div',
-            {
-              class: 'tostini',
-              attrs: {
-                'data-type': tostini.type
-              },
-              domProps
-            }
-          );
-        })
-      );
+    render: function render(h) {
+      return h('div', {
+        class: 'tostini-plate'
+      }, this.tostinis.map(function (tostini) {
+        var domProps = {};
+        domProps[tostini.html ? 'innerHTML' : 'innerText'] = tostini.message;
+        return h('div', {
+          class: 'tostini',
+          attrs: {
+            'data-type': tostini.type
+          },
+          domProps: domProps
+        });
+      }));
     }
   };
-
   /**
    * Bake
    */
+
   function bake(config) {
     // Handling config as string
     if (typeof config === 'string') {
       config = {
         message: config
       };
-    }
+    } // Calculate duration
 
-    // Calculate duration
+
     if (config.duration == null) {
-      config.duration = Math.max(
-        2000,
-        (1000 * config.message.length) / 12.84 + 400
-      );
-    }
+      config.duration = Math.max(2000, 1000 * config.message.length / 12.84 + 400);
+    } // Type
 
-    // Type
+
     if (config.type == null) {
       config.type = 'default';
-    }
+    } // HTML support?
 
-    // HTML support?
-    config.html = !!config.html;
 
-    // Emit
+    config.html = !!config.html; // Emit
+
     bus.$emit('tostini-bake', config);
   }
-
   /**
    * Plugin
    *
    * Definition of plugin itself
    */
-  const plugin = {
-    install(Vue$$1) {
-      // Define component
-      Vue$$1.component(component.name, component);
 
-      // Extend Vue prototype
+
+  var plugin = {
+    install: function install(Vue$$1) {
+      // Define component
+      Vue$$1.component(component.name, component); // Extend Vue prototype
+
       Vue$$1.prototype.$tostini = bake;
     }
   };
-
   return plugin;
-
-})));
+});
