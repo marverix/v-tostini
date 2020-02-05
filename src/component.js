8c57d2e538a19ee3edd81d3be9e66c2f3e6dde8e
Marek Sierociński <mareksierocinski@gmail.com>
2020-02-05 11:00:13 +0100

    Added some comment to render function

--- src/component.js
+++ src/component.js
@@ -74,8 +74,16 @@ const component = {
   render(h) {
     const { default: slot } = this.$scopedSlots;
 
+    /**
+     * @see https://vuejs.org/v2/guide/render-function.html#createElement-Arguments
+     */
     return h(
+      // {String | Object | Function}
+      // An HTML tag name, component options, or async function resolving to one of these
       'transition-group',
+
+      // {Object}
+      // A data object corresponding to the attributes
       {
         class: 'tostini-plate',
         props: {
@@ -83,19 +91,29 @@ const component = {
           tag: 'div'
         }
       },
+
+      // {String | Array}
+      // Children VNodes, built using `createElement()`, or using strings to get 'text VNodes'.
       this.tostinis.map(({ id, type, message, html }) => {
+        // The same as above :)
         return h(
           'div',
+
           {
             class: 'tostini',
             key: id,
             attrs: {
               'data-type': type
             },
+
+            /**
+             * @see https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
+             */
             domProps: !slot && {
               [html ? 'innerHTML' : 'innerText']: message
             }
           },
+
           slot && slot({
             type,
             message,
